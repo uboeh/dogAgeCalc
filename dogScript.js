4385dcdbd797dfd3721f184402c6fc6154b925b2
uboeh <ulrichboehlein@outlook.de>
2025-09-24 13:38:03 +0200

    Update dogScript.js

--- dogScript.js
+++ dogScript.js
@@ -1,5 +1,5 @@
 function hundeAlter() {
   var dogAge = prompt("How old is your dog?");
   var humanAge = (dogAge - 2) * 4 + 21;
-  alert("Your dog is " + humanAge + " years old.");
+  alert("Dein Hund ist " + humanAge + " Jahre alt.");
 }
